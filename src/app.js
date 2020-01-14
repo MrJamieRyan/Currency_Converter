c6f83e5effb1ae26857f127ca4835ede9b7a1d95
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-14 20:36:02 +0000

    Convert to EUR works

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
-import Vue from 'vue';
+import Vue from "vue";
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Vue({
-    el: "#app",
-    data: {
-
-    },
-    methods: {
-
-    }
-  });
+document.addEventListener("DOMContentLoaded", () => {
+	new Vue({
+		el: "#app",
+		data: {
+			currencies: null,
+			selectedCurrency: null,
+			euros: null
+		},
+		mounted() {
+			this.fetchCurrency();
+		},
+		methods: {
+			fetchCurrency: function() {
+				fetch("https://api.exchangeratesapi.io/latest")
+					.then(req => req.json())
+					.then(data => (this.currencies = data.rates));
+				this.currencies = null;
+			},
+			convertCurrency: function(currency, euros) {
+				const convertedAmount = currency * euros;
+				return convertedAmount;
+			}
+		}
+	});
 });
